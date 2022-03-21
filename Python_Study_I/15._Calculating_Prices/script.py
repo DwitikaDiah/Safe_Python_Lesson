226ae6cf29ef0d5b34e97ed8f3b77e441fa29205
Dwitika Diah Pangestuti <tikatikiww94@gmail.com>
2022-03-21 19:17:03 +0700

    Add files via upload

---
+++ Python_Study_I/15._Calculating_Prices/script.py
@@ -0,0 +1,17 @@
+# Assign 2 to apple_price variable
+apple_price = 2
+
+# Assign 5 to count variable
+count = 5
+
+# Assign the result of apple_price * count to total_price variable
+total_price = apple_price * count
+
+# By using the count variable, print 'You will buy .. apples'
+print('You will buy ' + str(count) + ' apples')
+
+# By using the total_price variable, print 'Your total is .. dollars'
+print('The total price is ' + str(total_price) + ' dollars')
+
+
+
---
+++ Python_Study_I/9._Data_Types/script.py
@@ -0,0 +1,18 @@
+age = 24
+# Print 'I am 24 years old' using the age variable
+print("Saya berusia "+str(age)+" tahun")
+
+count = '5'
+# Convert the count variable to an integer data type, add 1 to it, and print it
+total = 1 + int(count)
+print(total)
+
+
+
+
+
+
+
+
+
+
